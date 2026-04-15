2a460e8cf8981ede1897e25608686e7972ed7e6a
eunseolim <eunseolim1018@gmail.com>
2026-04-16 01:33:45 +0900

    feat: 스프링 환경 세팅

---
+++ src/test/java/com/tinypay/tinypay/TinypayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tinypay.tinypay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinypayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
