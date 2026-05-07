28d3f6811c6ba245dc968fde61c5779bef77617c
eeddiinn <gdw4013@naver.com>
2026-05-08 02:13:40 +0900

    [INIT] 프로젝트 초기 생성

---
+++ src/main/java/com/tinypay/TinypayBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tinypay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinypayBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TinypayBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tinypay/TinypayBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tinypay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinypayBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
