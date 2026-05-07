fb33d58cc1ee8524558574d6b406ba6eb5cc2184
eeddiinn <gdw4013@naver.com>
2026-05-08 04:59:24 +0900

    :wrench: [ADD] API 공통 응답 및 성공&에러 Enum 추가

---
+++ src/main/java/com/tinypay/global/response/SuccessType.java
@@ -0,0 +1,24 @@
+package com.tinypay.global.response;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public enum SuccessType {
+
+    /**
+     * HTTP 200 OK
+     */
+    PROCESS_SUCCESS(HttpStatus.OK, "OK"),
+    ;
+
+    private final HttpStatus httpStatus;
+    private final String message;
+
+    public int getHttpStatusCode() {
+        return httpStatus.value();
+    }
+}
