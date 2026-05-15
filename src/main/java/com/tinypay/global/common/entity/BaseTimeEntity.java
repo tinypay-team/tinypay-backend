f1df46d8228cfb64784561df07ce2aa005255d2d
eeddiinn <gdw4013@naver.com>
2026-05-15 11:05:47 +0900

    :wrench: [FIX] BaseTimeEntity 수정

--- src/main/java/com/tinypay/global/common/entity/BaseTimeEntity.java
+++ src/main/java/com/tinypay/global/common/entity/BaseTimeEntity.java
@@ -1,21 +1,26 @@
 package com.tinypay.global.common.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
+@Getter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public abstract class BaseTimeEntity {
 
     @CreatedDate // 엔티티 생성시 자동으로 값 설정
+    @Column(name = "created_at", nullable = false, updatable = false)
     private LocalDateTime createdAt;
 
     @LastModifiedDate // 엔티티 값 변경시 자동으로 값 갱신
+    @Column(name = "updated_at", nullable = false)
     private LocalDateTime updatedAt;
 
 }
