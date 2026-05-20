6a70a7bf4b9d1917535568ac011474a481df737d
eunseolim <eunseolim1018@gmail.com>
2026-05-20 17:41:47 +0900

    feat: 구글 소셜 로그인 구현

---
+++ src/main/java/com/tinypay/auth/dto/response/LoginResponse.java
@@ -0,0 +1,23 @@
+package com.tinypay.auth.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class LoginResponse {
+
+    private String accessToken;
+
+    private UserInfo user;
+
+    @Getter
+    @Builder
+    public static class UserInfo {
+        private Long userId;
+        private String email;
+        private String nickname;
+        private String profileImage;
+    }
+}
--- src/main/java/com/tinypay/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/tinypay/global/exception/GlobalExceptionHandler.java
@@ -1,16 +1,20 @@
 package com.tinypay.global.exception;
 
 import com.tinypay.global.response.ApiResponse;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -58,6 +62,6 @@ public class GlobalExceptionHandler {
     protected ResponseEntity<ApiResponse<Exception>> handleException(final Exception e) {
 
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                   .body(ApiResponse.error(INTERNAL_SERVER_ERROR, e));
+                .body(ApiResponse.error(INTERNAL_SERVER_ERROR, e));
     }
 }
